5b5c4288e7b92507cb43c66c3a723cc3492daa73
Cyril Gouverneur <cyril@imac-de-cyril.home>
2021-07-01 11:08:13 +0200

    Select random word

--- js/cg-english-hangman.js
+++ js/cg-english-hangman.js
@@ -1,6 +1,12 @@
 class cgEnglishHangman {
     constructor() {
-        this.parentEl = document.getElementById('cg-english-hangman')
+        this.parentEl     = document.getElementById('cg-english-hangman')
+        this.wrongLetters = document.getElementById('wrong-letters')
+        this.word         = document.getElementById('word')
+        this.popup        = document.getElementById('popup')
+        this.notification = document.getElementById('notification-container')
+        this.bodyParts    = document.querySelectorAll('.figure-part')
+
         this.restUrl  = this.parentEl.dataset.url
 
         this.getWords()
@@ -13,8 +19,18 @@ class cgEnglishHangman {
         .then(res => res.json())
         .then(res => {
             const words = res
+
+            const word = this.selectRandomWord(words)
+
+            console.log(word)
         })
         .catch(err => console.log(err))
+    }
+
+    selectRandomWord(words) {
+        const randomWord = words[Math.floor(Math.random() * words.length)].title
+
+        return randomWord
     }
 }
 
